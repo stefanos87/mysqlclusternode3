0c528761a33448bfa966763470074557b0be2221
stefanos87 <stefano.stirati@gmail.com>
2019-11-13 11:41:21 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "Start mysql"
 /etc/init.d/mysql start -D FOREGROUND
-
+sleep 120
+mysql -uroot -proot \
+  -e "GRANT ALL ON *.* TO 'root'@'%.%.%.%' identified by 'root';" \
+  -e "GRANT ALL ON *.* TO 'root'@'localhost' identified by 'root';" 
+  
 # sleep 120
 
 # mysql -uroot -proot \
